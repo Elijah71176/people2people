c7410433c7270bdf84621664d13d039fabf69e0e
elijah71176 <elijah71176@gmail.com>
2025-08-11 19:03:46 +0200

    intial commit
    wq

---
+++ src/main/java/se/elijah/people2people/People2peopleApplication.java
@@ -0,0 +1,13 @@
+package se.elijah.people2people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class People2peopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(People2peopleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/elijah/people2people/People2peopleApplicationTests.java
@@ -0,0 +1,13 @@
+package se.elijah.people2people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class People2peopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
